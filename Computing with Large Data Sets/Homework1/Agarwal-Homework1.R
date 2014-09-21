65c89830594a800676a5780facbeb5a7ed5da448
Abhi Agarwal <me@abhiagarwal.com>
2014-09-21 14:09:51 -0400

    Completed Homework

--- Computing with Large Data Sets/Homework1/Agarwal-Homework1.R
+++ Computing with Large Data Sets/Homework1/Agarwal-Homework1.R
@@ -10,51 +10,52 @@ rownames(bg.surv) <- bg.surv[,1]
 	
 # Goes through for just you
 currentPerson <- bg.surv[23, 5:21]
-similaritesList <- double()
 
-for(x in 1:42){
-	if(x != 23){
-		currentCompare <- cor(unlist(currentPerson), unlist(bg.surv[x, 5:21]))
-		similaritesList <- c(similaritesList, currentCompare*100)
-		if(closetstPerson < currentCompare){
-			closetstPerson = currentCompare
-			closetstPersonIndex = x
-		}
-		if(furthestPerson > currentCompare){
-			furthestPerson = currentCompare
-			furthestPersonIndex = x
+runHistogram <- function(usingSpearman = FALSE){
+
+	# Initialization of variables
+	closetstPerson <- -1000
+	closetstPersonIndex <- 0
+	furthestPerson <- 1000
+	furthestPersonIndex <- 0
+	similaritesList <- double()
+
+	if(usingSpearman) 
+		cat("Approach One: using spearman", "\n")
+	else 
+		cat("Approach One: without using spearman", "\n")
+
+	# Goes through each individual, but me.
+	for(x in 1:42){
+		if(x != 23){
+			# Compares using the cor function with each individual and me.
+			# Either of these will use spearman or normal:
+			if(usingSpearman) 
+				currentCompare <- cor(unlist(currentPerson), unlist(bg.surv[x, 5:21]), method = "spearman")
+			else 
+				currentCompare <- cor(unlist(currentPerson), unlist(bg.surv[x, 5:21]))
+			
+			similaritesList <- c(similaritesList, currentCompare * 100)
+			if(closetstPerson < currentCompare){
+				closetstPerson = currentCompare
+				closetstPersonIndex = x
+			}
+			if(furthestPerson > currentCompare){
+				furthestPerson = currentCompare
+				furthestPersonIndex = x
+			}
 		}
 	}
+
+	cat("Most similar to me is",  toString(bg.surv[closetstPersonIndex, 1:1]), "\n")
+	cat("Most dissimilar to me is",  toString(bg.surv[furthestPersonIndex, 1:1]), "\n")
+
+	if(usingSpearman) 
+		hist(similaritesList, xlim = c(0, 100), main = "Histogram: Similarity with spearman", xlab = "Percentage Similarity")
+	else 
+		hist(similaritesList, xlim = c(0, 100), main = "Histogram: Similarity without spearman", xlab = "Percentage Similarity")
 }
 
-cat("Closest person to me is",  toString(bg.surv[closetstPersonIndex, 1:1]), "\n")
-cat("Furthest person to me is",  toString(bg.surv[furthestPersonIndex, 1:1]))
-
-hist(similaritesList, xlim=c(0, 100))
-
-# Goes through each person
-# We are only comparing bg.surv[1, 5:21]
-# for(i in 1:42){
-	# closetstPerson <- -1000
-	# closetstPersonIndex <- 0
-	# furthestPerson <- 1000
-	# furthestPersonIndex <- 0
-	# currentPerson <- bg.surv[i, 5:21]
-	# for(x in 1:42){
-		# if(x != i){
-			# currentCompare <- cor(unlist(currentPerson), unlist(bg.surv[x, 5:21]))
-			# if(closetstPerson < currentCompare){
-				# closetstPerson = currentCompare
-				# closetstPersonIndex = x
-			# }
-			# if(furthestPerson > currentCompare){
-				# furthestPerson = currentCompare
-				# furthestPersonIndex = x
-			# }
-		# }
-	# }
-# }
-
-#for(y in 1:22){
-#	print(bg.surv[x, y:y])
-#}
+runHistogram(usingSpearman = TRUE)
+cat("\n")
+runHistogram(usingSpearman = FALSE)
