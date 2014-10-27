149fcac656d21ec3fa4a3c0e537f9ec5c3f30f0c
Abhi Agarwal <me@abhiagarwal.com>
2014-10-26 21:30:53 -0400

    Adding to README

--- Computing with Large Data Sets/Homework2/Agarwal-Homework2.R
+++ Computing with Large Data Sets/Homework2/Agarwal-Homework2.R
@@ -1,22 +1,28 @@
 library(lasso2)
 
+mainDirectory <- "/Users/abhi/Desktop/class/Computing with Large Data Sets/Homework2/"
+ratiosDirectory <- paste(mainDirectory, "baa.ratios.rda", sep="")
+annoTransDirectory <- paste(mainDirectory, "baa.anno.trans.rda", sep="")
+annoListDirectory <- paste(mainDirectory, "baa.anno.list.rda", sep="")
+listOfGenesDirectory <- paste(mainDirectory, "baa.TFs.txt", sep="")
+
 # ratios =  Matrix of genes - a gene is a row in this matrix.
-attach("/Users/abhi/Desktop/class/Computing with Large Data Sets/Homework2/baa.ratios.rda")
+attach(ratiosDirectory)
 
 # anno.trans
-attach("/Users/abhi/Desktop/class/Computing with Large Data Sets/Homework2/baa.anno.trans.rda")
+attach(annoTransDirectory)
 
 # anno.list
-attach("/Users/abhi/Desktop/class/Computing with Large Data Sets/Homework2/baa.anno.list.rda")
+attach(annoListDirectory)
 
 # listOfGenes = list of the genes that can be predictors.
-listOfGenes = as.matrix(read.table("/Users/abhi/Desktop/class/Computing with Large Data Sets/Homework2/baa.TFs.txt",
-	what = "character",
+listOfGenes = as.matrix(read.table(listOfGenesDirectory,
 	sep = "\n"
 ))
 
-# Takes a character and splits it
+# Takes a character and splits it, it splits it by space, ie: " ", but any multiple of it
 splitCharacter <- function(currentRow) return(strsplit(currentRow, " +")[[1]])
+
 
 # listPredictors = list of predictors
 # matrixName = matrix with name rows
--- README.md
+++ README.md
@@ -2,3 +2,5 @@ Classes
 =======
 
 All the coding work from my classes at NYU for Spring 2014, Fall 2014.
+
+Class work with C++, Java, Bash, Hacs built ontop of CRSX (http://crsx.org/), R, Python, Arduino, Node.JS, Javascript, and Matlab.
