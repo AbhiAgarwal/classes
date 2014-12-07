6dd3aa24c5cc1fb41933a843ff263ad270a015ac
Abhi Agarwal <me@abhiagarwal.com>
2014-12-07 01:27:01 -0500

    Beginning documentation

--- Computing with Large Data Sets/Homework3/Agarwal-Homework3.py
+++ Computing with Large Data Sets/Homework3/Agarwal-Homework3.py
@@ -16,68 +16,94 @@ def sentimentAnalysis(text):
 	blob = TextBlob(text)
 	return blob.sentiment.polarity
 
+def wordFunction(csvFileDataset):
+	# Check the words, and remove english words
+	reducedList = []
+	nonEnglishWords = 0
+	amountOfGunned = 0
+	for i in csvFileDataset:
+		if eachWord(i[0]):
+			reducedList.append(i)
+		else:
+			nonEnglishWords += 1
+		amountOfGunned += 1
+
+	print "Number of words:", amountOfGunned
+	print "Number of words not english foreach letter:", nonEnglishWords
+
+	# How many words not in english dictionary
+	reducedList2 = []
+	notInEnglishDictionary = 0
+	for i in reducedList:
+		if not d.check(i[0]):
+			notInEnglishDictionary += 1
+		else:
+			reducedList2.append(i)
+
+	print "Number of words not in english dictionary", notInEnglishDictionary
+	return reducedList2
+
+def sentimentFunction(reducedList2):
+	sectionOne, sectionTwo, sectionThree, sectionFour = 0.0, 0.0, 0.0, 0.0
+	for i in reducedList2[1:len(reducedList2)]:
+		sentimentOfWord = float(sentimentAnalysis(i[0]))
+		sectionOne += (sentimentOfWord * float(i[1]))
+		sectionTwo += (sentimentOfWord * float(i[2]))
+		sectionThree += (sentimentOfWord * float(i[3]))
+		sectionFour += (sentimentOfWord * float(i[4]))
+	sectionOne /= len(reducedList2)
+	sectionTwo /= len(reducedList2)
+	sectionThree /= len(reducedList2)
+	sectionFour /= len(reducedList2)
+	seq = [sectionOne, sectionTwo, sectionThree, sectionFour]
+	ifDescendending = all(earlier >= later for earlier, later in zip(seq, seq[1:]))
+	ifAscending = all(earlier <= later for earlier, later in zip(seq, seq[1:]))
+
+	if not ifAscending and not ifDescendending:
+		print 'Sentiment fluctuates over time'
+	else:
+		if ifDescendending:
+			print 'Sentiment is descending over time'
+		if ifAscending:
+			print 'Sentiment is ascending over time'
+
 # ['term', '2014-10-20', '2014-10-27', '2014-10-06', '2014-10-13']
 ###########
+
+print 'Ebola'
 
 # Ebola
 ebola = open('Ebola_frequencies.csv')
 csv_ebola = csv.reader(ebola)
-
-# Check the words, and remove english words
-reducedList = []
-nonEnglishWords, amountOfEbola = 0, 0
-for i in csv_ebola:
-	if eachWord(i[0]):
-		reducedList.append(i)
-	else:
-		nonEnglishWords += 1
-	amountOfEbola += 1
-
-print "Number of words:", amountOfEbola
-print "Number of words not english foreach letter:", nonEnglishWords
-
-# How many words not in english dictionary
-reducedList2 = []
-notInEnglishDictionary = 0
-for i in reducedList:
-	if not d.check(i[0]):
-		notInEnglishDictionary += 1
-	else:
-		reducedList2.append(i)
-
-print "Number of words not in english dictionary", notInEnglishDictionary
+reducedList2 = wordFunction(csv_ebola)
 
 # Sentimental Analysis
-sectionOne, sectionTwo, sectionThree, sectionFour = 0.0, 0.0, 0.0, 0.0
-for i in reducedList2[1:len(reducedList2)]:
-	sentimentOfWord = float(sentimentAnalysis(i[0]))
-	sectionOne += (sentimentOfWord * float(i[1]))
-	sectionTwo += (sentimentOfWord * float(i[2]))
-	sectionThree += (sentimentOfWord * float(i[3]))
-	sectionFour += (sentimentOfWord * float(i[4]))
-sectionOne /= len(reducedList2)
-sectionTwo /= len(reducedList2)
-sectionThree /= len(reducedList2)
-sectionFour /= len(reducedList2)
-
-seq = [sectionOne, sectionTwo, sectionThree, sectionFour]
-ifDescendending = all(earlier >= later for earlier, later in zip(seq, seq[1:]))
-
-if ifDescendending:
-	print 'Sentiment is descending over time'
-else:
-	print 'Sentiment is ascending over time'
+sentimentFunction(reducedList2)
 
 ###########
+
+print ''
+print 'If They Gunned Me Down'
 
 # IfTheyGunnedMeDown_frequencies
-gunned = open('Ebola_frequencies.csv')
+gunned = open('IfTheyGunnedMeDown_frequencies.csv')
 csv_gunned = csv.reader(gunned)
+reducedList2 = wordFunction(csv_gunned)
+
+# Sentimental Analysis
+sentimentFunction(reducedList2)
 
 ###########
 
+print ''
+print 'US Top 10 Cities'
+
 # USTop10Cities_frequencies
-city = open('Ebola_frequencies.csv')
+city = open('USTop10Cities_frequencies.csv')
 csv_city = csv.reader(city)
+reducedList2 = wordFunction(csv_city)
+
+# Sentimental Analysis
+sentimentFunction(reducedList2)
 
 ###########
--- Computing with Large Data Sets/Homework3/Answers.md
+++ Computing with Large Data Sets/Homework3/Answers.md
@@ -1,13 +1,8 @@
-Firstly I only filtered words that were in english - so I used a function to remove and normalize the datasets to only having english words.
-
 #### Ebola
 
 1. what is different about the distribution of word frequencies ?
 2. what are the most enriched or depleted words in each set
 3. could you perform automated sentiment analysis of the word lists (by sampling words from this frequency) and estimate the change in sentiment by week.
-
-	- 
-
 4. if you collect tweets today have the word count frequencies changed ?
 5. can you identify dates and places separate from other words and determine relative frequencies of dates
 6. fraction of words that are proper nouns or names change from event to event or by time?
