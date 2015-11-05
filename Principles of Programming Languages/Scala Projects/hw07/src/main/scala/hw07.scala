408c6a7faf53233f51af2d56376606eb4c34b10f
Abhi Agarwal <me@abhiagarwal.com>
2015-11-05 14:56:35 -0500

    4 tests down

--- Principles of Programming Languages/Scala Projects/hw07/src/main/scala/hw07.scala
+++ Principles of Programming Languages/Scala Projects/hw07/src/main/scala/hw07.scala
@@ -30,71 +30,89 @@ object hw07 extends js.util.JsApp {
    */
 
   /* Collections and Higher-Order Functions */
-  
+
   /* Lists */
-  
+
+  // That eliminates consecutive duplicates of list elements.
+  // If a list contains repeated elements they should be replaced with a single copy of the element.
+  // The order of the elements should not be changed.
   def compressRec[A](l: List[A]): List[A] = l match {
     case Nil | _ :: Nil => l
-    case h1 :: (t1 @ (h2 :: _)) => 
+    case h1 :: (t1 @ (h2 :: _)) =>
       if (h1 == h2) compressRec(t1)
       else h1 :: compressRec(t1)
   }
-  
-  def compressFold[A](l: List[A]): List[A] = l.foldRight(Nil: List[A]){
-    (h, acc) => acc match {
-      case h1 :: _ => if (h1 == h) acc else h :: acc
-      case _ => h :: acc
+
+  // Re implement the compress function from the previous part as compressFold using the foldRight method from the List class in the standard library.
+  // The call to foldRight has been provided for you. 
+  // Do not call compressFold recursively or any other List methods.
+  def compressFold[A](l: List[A]): List[A] = l.foldRight(Nil: List[A]) {
+    (h, acc) =>
+      acc match {
+        case h1 :: _ => if (h1 == h) acc else h :: acc
+        case _ => h :: acc
+      }
+  }
+
+  // that finds the first element in l where f applied to it returns a Some(a) for some value a.
+  // It should replace that element with a and leave l the same everywhere else.
+  def mapFirst[A](f: A => Option[A])(l: List[A]): List[A] = l match {
+    case Nil => Nil
+    case h :: t => f(h) match {
+      case Some(a) => a :: t
+      case _ => h :: mapFirst(f)(t)
     }
   }
-  
-  def mapFirst[A](f: A => Option[A])(l: List[A]): List[A] = l match {
-    case Nil => ???
-    case h :: t => ???
-  }
-  
+
   /* Search Trees */
-  
+
   sealed abstract class Tree {
     def insert(n: Int): Tree = this match {
       case Empty => Node(Empty, n, Empty)
       case Node(l, d, r) => if (n < d) Node(l insert n, d, r) else Node(l, d, r insert n)
-    } 
-    
+    }
+
+    // Here, we have implemented the binary search tree operation insert as a method of Tree.
+    // For this exercise, complete the higher-order method foldLeft.
+    // This method performs an in-order traversal of the input tree this calling the callback f to accumulate a result.
+    // Suppose the in-order traversal of the input tree yields the following sequence of data values: d1, d2, ..., dn.
     def foldLeft[A](z: A)(f: (A, Int) => A): A = {
       def loop(acc: A, t: Tree): A = t match {
-        case Empty => ???
-        case Node(l, d, r) => ???
+        case Empty => acc
+        // left: Tree, d: Int, right: Tree
+        case Node(l, d, r) => f(loop(acc, r), d)
       }
       loop(z, this)
     }
-    
+
     def pretty: String = {
       def p(acc: String, t: Tree, indent: Int): String = t match {
         case Empty => acc
         case Node(l, d, r) =>
           val spacer = " " * indent
           p("%s%d%n".format(spacer, d) + p(acc, l, indent + 2), r, indent + 2)
-      } 
+      }
       p("", this, 0)
     }
   }
   case object Empty extends Tree
   case class Node(l: Tree, d: Int, r: Tree) extends Tree
-  
+
   def treeFromList(l: List[Int]): Tree =
-    l.foldLeft(Empty: Tree){ (acc, i) => acc insert i }
-  
-  def sum(t: Tree): Int = t.foldLeft(0){ (acc, d) => acc + d }
-  
+    l.foldLeft(Empty: Tree) { (acc, i) => acc insert i }
+
+  def sum(t: Tree): Int = t.foldLeft(0) { (acc, d) => acc + d }
+
+  // As a client of your foldLeft method that checks whether the data values of t as an in-order traversal are in strictly ascending order (i.e., d1 < d2 < ... < dn).
   def strictlyOrdered(t: Tree): Boolean = {
-    val (b, _) = t.foldLeft((true, None: Option[Int])){
+    val (b, _) = t.foldLeft((true, None: Option[Int])) {
       ???
     }
     b
   }
-  
+
   /* JakartaScript */
-  
+
   def toNum(v: Val): Double = {
     v match {
       case Num(n) => n
@@ -105,7 +123,7 @@ object hw07 extends js.util.JsApp {
       case Function(_, _, _) => Double.NaN
     }
   }
-  
+
   def toBool(v: Val): Boolean = {
     v match {
       case Num(n) if (n compare 0.0) == 0 || (n compare -0.0) == 0 || n.isNaN => false
@@ -117,7 +135,7 @@ object hw07 extends js.util.JsApp {
       case Function(_, _, _) => true
     }
   }
-  
+
   def toStr(v: Val): String = {
     v match {
       case Num(n) => if (n.isWhole) "%.0f" format n else n.toString
@@ -127,13 +145,13 @@ object hw07 extends js.util.JsApp {
       case Function(_, _, _) => "function"
     }
   }
-  
+
   /*
    * Helper function that implements the semantics of inequality
    * operators Lt, Le, Gt, and Ge on values.
    */
   def inequalityVal(bop: Bop, v1: Val, v2: Val): Boolean = {
-  require(bop == Lt || bop == Le || bop == Gt || bop == Ge)
+    require(bop == Lt || bop == Le || bop == Gt || bop == Ge)
     (v1, v2) match {
       case (Str(s1), Str(s2)) =>
         (bop: @unchecked) match {
@@ -152,7 +170,7 @@ object hw07 extends js.util.JsApp {
         }
     }
   }
-    
+
   /* 
    * Substitutions e[v/x]
    */
@@ -169,16 +187,14 @@ object hw07 extends js.util.JsApp {
       case UnOp(uop, e1) => UnOp(uop, substX(e1))
       case BinOp(bop, e1, e2) => BinOp(bop, substX(e1), substX(e2))
       case If(b, e1, e2) => If(substX(b), substX(e1), substX(e2))
-      case ConstDecl(y, ed, eb) => 
+      case ConstDecl(y, ed, eb) =>
         ConstDecl(y, substX(ed), if (x == y) eb else substX(eb))
-      case Call(e0, es) => 
-        ???
-      case Function(p, ys, eb) => 
+      case Call(e0, es) => Call(substX(e0), es map substX)
+      case Function(p, ys, eb) =>
         ???
     }
   }
 
-  
   /*
    * This code is a reference implementation of JakartaScript without
    * functions and big-step static binding semantics.
@@ -191,38 +207,40 @@ object hw07 extends js.util.JsApp {
     e match {
       /* Base Cases */
       case v: Val => v
-      
+
       /* Inductive Cases */
-      case Print(e) => println(eval(e).prettyVal); Undefined
+      case Print(e) =>
+        println(eval(e).prettyVal); Undefined
 
-      case UnOp(UMinus, e1) => Num(- eToNum(e1))
+      case UnOp(UMinus, e1) => Num(-eToNum(e1))
 
-      case UnOp(Not, e1) => Bool(! eToBool(e1))
-      
+      case UnOp(Not, e1) => Bool(!eToBool(e1))
+
       case BinOp(Plus, e1, e2) => (eval(e1), eval(e2)) match {
         case (Str(s1), v2) => Str(s1 + toStr(v2))
         case (v1, Str(s2)) => Str(toStr(v1) + s2)
         case (v1, v2) => Num(toNum(v1) + toNum(v2))
-      }      
+      }
       case BinOp(Minus, e1, e2) => Num(eToNum(e1) - eToNum(e2))
-      
+
       case BinOp(Times, e1, e2) => Num(eToNum(e1) * eToNum(e2))
-      
+
       case BinOp(Div, e1, e2) => Num(eToNum(e1) / eToNum(e2))
-      
-      case BinOp(And, e1, e2) => 
+
+      case BinOp(And, e1, e2) =>
         val v1 = eval(e1)
         if (toBool(v1)) eval(e2) else v1
-      
+
       case BinOp(Or, e1, e2) =>
         val v1 = eval(e1)
         if (toBool(v1)) v1 else eval(e2)
-      
-      case BinOp(Seq, e1, e2) => eval(e1); eval(e2)
-      
+
+      case BinOp(Seq, e1, e2) =>
+        eval(e1); eval(e2)
+
       case BinOp(bop, e1, e2) =>
         bop match {
-          case Eq | Ne => 
+          case Eq | Ne =>
             def checkFun(v: Expr) = v match {
               case Function(_, _, _) => throw DynamicTypeError(e)
               case _ => ()
@@ -232,21 +250,21 @@ object hw07 extends js.util.JsApp {
             val v2 = eval(e2)
             checkFun(v2)
             (bop: @unchecked) match {
-               case Eq => Bool(v1 == v2)
-               case Ne => Bool(v1 != v2)
+              case Eq => Bool(v1 == v2)
+              case Ne => Bool(v1 != v2)
             }
           case _ => Bool(inequalityVal(bop, eval(e1), eval(e2)))
         }
-              
+
       case If(e1, e2, e3) => if (eToBool(e1)) eval(e2) else eval(e3)
-      
+
       case ConstDecl(x, ed, eb) => eval(subst(eb, x, eval(ed)))
-      
-      case Call(e0, es) => 
+
+      case Call(e0, es) =>
         // TODO: implement the EvalCall and EvalCallRec rules
         val v0 = eval(e0)
         v0 match {
-          case Function(p, xs, eb) => 
+          case Function(p, xs, eb) =>
             val ebp = p match {
               case None => eb
               case Some(x0) => subst(eb, x0, v0)
@@ -254,39 +272,39 @@ object hw07 extends js.util.JsApp {
             // evaluate es and extend result with Undefined values if es.size < xs.size
             val vs_padded = ???
             // compute common substitutions for EvalCall and EvalCallRec rules
-            val ebpp = (xs, vs_padded).zipped.foldRight(ebp){
+            val ebpp = (xs, vs_padded).zipped.foldRight(ebp) {
               case ((xi, vi), ebpp) => ???
             }
             eval(ebpp)
           case _ => throw DynamicTypeError(e)
         }
-        
+
       case Var(_) => throw StuckError(e) // this should never happen
     }
   }
-   
+
   // Interface to run your interpreter from a string.  This is convenient
   // for unit testing.
   def evaluate(s: String): Val = eval(parse.fromString(s))
-    
-  /* Interface to run your interpreter from the command line.  You can ignore the code below. */ 
-  
+
+  /* Interface to run your interpreter from the command line.  You can ignore the code below. */
+
   def processFile(file: java.io.File) {
     if (debug) {
       println("============================================================")
       println("File: " + file.getName)
       println("Parsing ...")
     }
-    
+
     val expr = handle(fail()) {
       parse.fromFile(file)
     }
-      
+
     if (debug) {
       println("Parsed expression:")
       println(expr)
-    }  
-    
+    }
+
     handle() {
       val v = eval(expr)
       println(v.prettyVal)
